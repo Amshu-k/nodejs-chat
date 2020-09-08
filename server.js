bf67816bc77a911d30175a07692b931b7c768e0d
Amshu <k.amshuman@gmail.com>
2020-09-07 20:07:33 -0400

    npm init

---
+++ server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const http = require('http')
+const express = require('express');
+const socketio = require('socket.io')
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Run when a client connects
+io.on('connection', socket => {
+    console.log('new WS connection');
+}) 
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => {console.log('server running');})
